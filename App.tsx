c6a4366d43a0a48163ab7cb1055aa12e294923a9
Pablo Rodríguez <prodrigu75@gmail.com>
2021-09-25 13:09:09 -0300

    Added basic global style and Calculator screen

--- App.tsx
+++ App.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import { SafeAreaView, StatusBar, StyleSheet } from 'react-native';
+import { CalculatorScreen } from './src/screens';
+import { AppTheme } from './src/theme';
 
 export default () => {
   return (
-    <View>
-      <Text>Calculator</Text>
-    </View>
+    <SafeAreaView style={ AppTheme.background }>
+      <StatusBar
+        backgroundColor="black"
+        barStyle="light-content"
+      />
+      <CalculatorScreen/>
+    </SafeAreaView>
   )
 }
---
+++ src/screens/Calculator.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { View, Text } from 'react-native';
+import { AppTheme } from '../theme';
+
+export const CalculatorScreen = () => {
+    return (
+        <View>
+            <Text style={ AppTheme.text }>
+                Calculator Screen
+            </Text>     
+        </View>
+    )
+}
