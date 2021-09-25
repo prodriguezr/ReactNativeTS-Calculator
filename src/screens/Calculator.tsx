ea724c90c7a5e706d5b39f68d58282ee94f5445a
Pablo Rodríguez <prodrigu75@gmail.com>
2021-09-25 13:45:51 -0300

    Added a button on the Calculator screen and customized results text (large and small)

--- src/screens/Calculator.tsx
+++ src/screens/Calculator.tsx
@@ -4,10 +4,16 @@ import { AppTheme } from '../theme';
 
 export const CalculatorScreen = () => {
     return (
-        <View>
-            <Text style={ AppTheme.text }>
-                Calculator Screen
-            </Text>     
+        <View style={ AppTheme.calculatorContainer }>
+            <Text style={ AppTheme.smallResult }>1,000.00</Text>     
+            <Text style={ AppTheme.result }>1,500.00</Text>  
+
+            <View>
+                {/* Button */}
+                <View style={ AppTheme.button }>
+                    <Text style={ AppTheme.textButton }>1</Text>
+                </View>
+            </View>   
         </View>
     )
 }
--- src/theme/AppTheme.tsx
+++ src/theme/AppTheme.tsx
@@ -5,8 +5,33 @@ export default StyleSheet.create({
         flex: 1,
         backgroundColor: 'black',
     },
-    text: {
+    result: {
         color: 'white',
+        fontSize: 80,
+        textAlign: 'right',
+    },
+    smallResult: {
+        color: 'rgba(255, 255, 255, 0.4)',
         fontSize: 30,
+        textAlign: 'right',
+    },
+    calculatorContainer: {
+        flex: 1,
+        paddingHorizontal: 22,
+        justifyContent: 'flex-end'
+    },
+    button: {
+        height: 100,
+        width: 100,
+        borderRadius: 100,
+        backgroundColor: '#9B9B9B',
+        color: 'white',
+        justifyContent: 'center'
+    },
+    textButton: {
+        fontSize: 40,
+        color: 'black',
+        textAlign: 'center',
+        fontWeight: 'bold',
     }
 });
